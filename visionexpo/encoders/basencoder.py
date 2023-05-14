0144b8a8fd052a62712b8c93f5756b6f2e801f95
Andrea Boscolo Camiletto <abcamiletto@gmail.com>
2023-05-14 13:48:51 +0200

    added comment

--- visionexpo/encoders/basencoder.py
+++ visionexpo/encoders/basencoder.py
@@ -5,13 +5,24 @@ from torch import Tensor
 
 
 class BaseEncoder(nn.Module):
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x: Tensor) -> list[Tensor]:
+        """Forward pass of the encoder
+
+        Args:
+            x: Input tensor of shape (B, C, H, W)
+
+        Returns:
+            A list of tensors of shape (B, C, H, W) with the features.
+            The first element is the input tensor.
+        """
         raise NotImplementedError
 
     @property
     def out_channels(self) -> tuple[int, ...]:
+        """Number of channels of the output tensors"""
         raise NotImplementedError
 
     @property
-    def out_strides(self) -> tuple[int, ...]:
+    def out_reductions(self) -> tuple[int, ...]:
+        """Reduction factor of the output tensors"""
         raise NotImplementedError
