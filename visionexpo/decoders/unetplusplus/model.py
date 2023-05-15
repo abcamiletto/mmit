3e4be7dcb0c4e0b122b787767459d32a34ed4a0e
Andrea Boscolo Camiletto <abcamiletto@gmail.com>
2023-05-16 01:18:38 +0200

    reqriting of unet++

--- visionexpo/decoders/unetplusplus/model.py
+++ visionexpo/decoders/unetplusplus/model.py
@@ -1,3 +1,6 @@
+from functools import partial
+from typing import Type
+
 import torch
 import torch.nn as nn
 
@@ -9,124 +12,106 @@ DEFAULT_CHANNELS = (256, 128, 64, 32, 16)
 
 
 class UNetPlusPlus(BaseDecoder):
+    """UNet++ decoder implementation from https://arxiv.org/abs/1807.10165.
+
+    In this implementation, we follow the following naming convention referring to Figure 1.a in the paper:
+        - `lidx` is the layer index, i.e. the index that spans horizontally.
+        - `didx` is the depth index, i.e. the index that spans vertically.
+        - `i_j` will be the key of the block that is at depth `i` and layer `j`.
+        - `i_j` will also be the key of the resulting tensor after the block that is at depth `i` and layer `j`.
+
+    Since we implement only the decoder, there will be no `i_0` blocks, and the `i_0` tensors will be the input tensors.
+    """
+
     def __init__(
         self,
         input_channels: list[int],
         input_reductions: list[int],
         decoder_channels: list[int] = None,
-        upsample_layer: nn.Module = up.ConvTranspose2d,
-        norm_layer: nn.Module = nn.BatchNorm2d,
-        activation: nn.Module = nn.ReLU(inplace=True),
-        extra_layer: nn.Module = nn.Identity(),
+        upsample_layer: Type[nn.Module] = up.ConvTranspose2d,
+        norm_layer: Type[nn.Module] = nn.BatchNorm2d,
+        activation: Type[nn.Module] = nn.ReLU,
+        extra_layer: Type[nn.Module] = nn.Identity,
     ):
         super().__init__()
+        self.depth = len(input_channels)
         if decoder_channels is None:
-            decoder_channels = DEFAULT_CHANNELS[: len(input_channels) - 1]
+            decoder_channels = DEFAULT_CHANNELS[: self.depth - 1]
+            print(f"Using default decoder channels: {decoder_channels}")
 
-        in_ch, skip_ch, out_ch = self._format_channels(input_channels, decoder_channels)
-        self.in_channels = in_ch
-        self.skip_channels = skip_ch
-        self.out_channels = out_ch
-
-        # combine decoder keyword arguments
-        specs = upsample_layer, norm_layer, activation, extra_layer
+        channels = self._format_channels(input_channels, decoder_channels)
+        up_lays = self._format_upsample_layers(input_reductions, upsample_layer)
+        specs = norm_layer, activation, extra_layer
 
         blocks = {}
-        for layer_idx in range(len(self.in_channels) - 1):
-            for depth_idx in range(layer_idx + 1):
-                ic = in_ch[layer_idx] if depth_idx == 0 else skip_ch[layer_idx - 1]
-                sc = skip_ch[layer_idx] * (layer_idx + 1 - depth_idx)
-                oc = out_ch[layer_idx] if depth_idx == 0 else skip_ch[layer_idx]
-
-                bname = f"x_{depth_idx}_{layer_idx}"
-                blocks[bname] = UpBlock(ic, sc, oc, *specs)
-
-        # Add final block
-        bname = f"x_{0}_{len(in_ch)-1}"
-        blocks[bname] = UpBlock(in_ch[-1], 0, out_ch[-1], *specs)
+        for key, (in_ch, skip_ch, out_ch) in channels.items():
+            didx = int(key.split("_")[0])
+            up_lay = up_lays[didx]
+            blocks[key] = UpBlock(in_ch, skip_ch, out_ch, up_lay, *specs)
 
         self.blocks = nn.ModuleDict(blocks)
-        self.depth = len(in_ch) - 1
 
     @size_control
     def forward(self, *features):
         features = self._preprocess_features(features)
-        intermediate_outputs = self._generate_intermediate_outputs(features)
-        final_output = self._process_final_layer(intermediate_outputs)
+        for lidx in range(1, self.depth):
+            for didx in range(self.depth - lidx):
+                # Get the corresponding block
+                block = self.blocks[f"{didx}_{lidx}"]
+
+                input_tensor = features[f"{didx+1}_{lidx-1}"]
+                skip_tensors = [features[f"{didx}_{li}"] for li in range(lidx)]
+                skip_tensors = torch.cat(skip_tensors, dim=1)
+
+                # Apply the block to the input tensor
+                output = block(input_tensor, skip_tensors)
+
+                # Store the output in the dictionary
+                features[f"{didx}_{lidx}"] = output
+
+        # The final output is the output of the last block
+        final_output = features[f"0_{self.depth-1}"]
+
         return final_output
 
     def _preprocess_features(self, features):
         """Preprocess features by removing the first feature and reversing the order."""
-        features_without_first = features[1:]  # Remove first feature
-        reversed_features = features_without_first[::-1]  # Reverse feature order
-        return reversed_features
-
-    def _generate_intermediate_outputs(self, features):
-        """Generate intermediate outputs for each layer in the network."""
-        intermediate_outputs = {}  # Dictionary to store intermediate outputs
-
-        for layer_idx in range(len(self.in_channels) - 1):
-            for depth_idx in range(self.depth - layer_idx):
-                output_key = f"x_{depth_idx}_{depth_idx + layer_idx}"
-                output_value = self._process_layer(
-                    depth_idx, layer_idx, intermediate_outputs, features
-                )
-                intermediate_outputs[output_key] = output_value
-
-        return intermediate_outputs
-
-    def _process_layer(self, depth_idx, layer_idx, intermediate_outputs, features):
-        """Process a single layer in the network."""
-        if layer_idx == 0:
-            return self._process_first_layer(depth_idx, features)
-
-        concat_features = self._concatenate_features(
-            depth_idx, layer_idx, intermediate_outputs, features
-        )
-        prev_output_key = f"x_{depth_idx}_{depth_idx + layer_idx - 1}"
-        current_block_key = f"x_{depth_idx}_{depth_idx + layer_idx}"
-
-        prev_output = intermediate_outputs[prev_output_key]
-        current_block = self.blocks[current_block_key]
-
-        return current_block(prev_output, concat_features)
-
-    def _process_first_layer(self, depth_idx, features):
-        """Process the first layer in the network."""
-        input1 = features[depth_idx]
-        input2 = features[depth_idx + 1]
-        current_block = self.blocks[f"x_{depth_idx}_{depth_idx}"]
-        return current_block(input1, input2)
-
-    def _concatenate_features(self, depth_idx, layer_idx, inter_outputs, features):
-        """Concatenate intermediate features and the next feature in the sequence."""
-        intermediate_keys = [
-            f"x_{idx}_{depth_idx + layer_idx}"
-            for idx in range(depth_idx + 1, depth_idx + layer_idx + 1)
-        ]
-        intermediate_features = [inter_outputs[key] for key in intermediate_keys]
-        next_feature = features[depth_idx + layer_idx + 1]
-        return torch.cat(intermediate_features + [next_feature], dim=1)
-
-    def _process_final_layer(self, inter_outputs):
-        """Process the final layer in the network."""
-        final_input_key = f"x_{0}_{self.depth - 1}"
-        final_input = inter_outputs[final_input_key]
-        final_block = self.blocks[f"x_{0}_{self.depth}"]
-        return final_block(final_input)
+        features = {f"{i}_0": f for i, f in enumerate(features)}
+        return features
 
     def _format_channels(self, input_channels, decoder_channels):
-        # We drop the first channel since we don't use the input image
-        input_channels = input_channels[1:]
+        decoder_channels = decoder_channels[::-1]
 
-        # We reverse the input channels since we're going from the bottom up
-        input_channels = input_channels[::-1]
+        # We start by defining the input, skip and output channels for the first layer
+        in_channels = {f"{i}_1": c for i, c in enumerate(input_channels[1:])}
+        skip_channels = {f"{i}_1": c for i, c in enumerate(input_channels[:-1])}
+        out_channels = {f"{i}_1": c for i, c in enumerate(decoder_channels)}
 
-        # On the last layer we don't have a skip connection
-        skip_channels = input_channels[1:] + [0]
+        # We recursively define the input, skip and output channels for the other layers
+        for lidx in range(2, self.depth):
+            for didx in range(self.depth - lidx):
+                # We define the keys of the current block and its neighbors
+                current = f"{didx}_{lidx}"
+                bottom_left = f"{didx+1}_{lidx-1}"
+                left = f"{didx}_{lidx-1}"
 
-        # The first layer has the same number of channels as the input
-        # The rest has the output channels of the previous layer
-        in_channels = [input_channels[0]] + list(decoder_channels[:-1])
+                # We define the input, skip and output channels of the current block
+                in_channels[current] = out_channels[bottom_left]
+                skip_channels[current] = skip_channels[left] + out_channels[left]
+                out_channels[current] = out_channels[left]
 
-        return in_channels, skip_channels, decoder_channels
+        return {
+            k: (in_channels[k], skip_channels[k], out_channels[k]) for k in in_channels
+        }
+
+    def _format_upsample_layers(self, input_reductions, upsample_layer):
+        # We build a mask to filter out the layers that don't need upsampling
+        upsample_layers = []
+        for i in range(1, len(input_reductions)):
+            scale = input_reductions[i] // input_reductions[i - 1]
+
+            # Partially initialize the upsample layer with the scale
+            layer = partial(upsample_layer, scale=scale)
+            upsample_layers.append(layer)
+
+        return upsample_layers
