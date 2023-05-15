03f5b3df6b623030085e9e4c1f40b7fc93956383
Andrea Boscolo Camiletto <abcamiletto@gmail.com>
2023-05-15 21:48:56 +0200

    improved upsamplers with scale input

--- visionexpo/base/upsamplers.py
+++ visionexpo/base/upsamplers.py
@@ -1,23 +1,20 @@
 import torch.nn as nn
 
 
-class ConvTranspose2d(nn.Module):
-    def __init__(self, in_channels, out_channels=None):
-        super().__init__()
-        if out_channels is None:
-            out_channels = in_channels
+class Upsample(nn.Upsample):
+    def __init__(self, in_channels: int, scale: int = 2):
+        super().__init__(scale_factor=scale, mode="bilinear")
 
-        self.conv = nn.ConvTranspose2d(
-            in_channels, out_channels, kernel_size=2, stride=2
+
+class ConvTranspose2d(nn.ConvTranspose2d):
+    def __init__(
+        self,
+        in_channels: int,
+        scale: int = 2,
+    ):
+        super().__init__(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=scale,
+            stride=scale,
         )
-
-    def forward(self, x):
-        return self.conv(x)
-
-
-class Identity(nn.Module):
-    def __init__(self, in_channels, out_channels=None):
-        super().__init__()
-
-    def forward(self, x):
-        return x
