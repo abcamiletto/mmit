465d806eaa936bd93b27960a70b525a4b74087d0
Andrea Boscolo Camiletto <abcamiletto@gmail.com>
2023-05-28 17:45:27 +0200

    used main convnormact layer

--- mmit/decoders/fpn/parts.py
+++ mmit/decoders/fpn/parts.py
@@ -1,43 +1,9 @@
 from typing import Type
 
 import torch.nn as nn
-from torch import Tensor
 
 from mmit.base import mismatch as mm
 from mmit.base import upsamplers as up
-
-
-class ConvNormActivation(nn.Module):
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        upsample_layer: Type[nn.Module] = up.ConvTranspose2d,
-        norm_layer: Type[nn.Module] = nn.BatchNorm2d,
-        activation: Type[nn.Module] = nn.ReLU,
-        extra_layer: Type[nn.Module] = nn.Identity,
-    ) -> None:
-        super().__init__()
-
-        self.conv = nn.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size=3,
-            padding=1,
-            bias=False,
-        )
-        self.up = upsample_layer(in_channels)
-        self.norm = norm_layer(out_channels)
-        self.activation = activation()
-        self.extra = extra_layer()
-
-    def forward(self, x: Tensor) -> Tensor:
-        x = self.conv(x)
-        x = self.norm(x)
-        x = self.activation(x)
-        x = self.up(x)
-        x = self.extra(x)
-        return x
 
 
 class SkipBlock(nn.Module):
